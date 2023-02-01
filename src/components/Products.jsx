1f3dbdd3b976ad849fe20abdd5a38a1fc0f21173
jy0813 <busbug3@gmail.com>
2023-02-01 22:01:03 +0900

    feat: dev-tools 적용 및 staleTime 및 cache 컨트롤

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,10 +3,12 @@ import {useQuery} from "@tanstack/react-query";
 
 export default function Products() {
   const [checked, setChecked] = useState(false);
-  const {isLoading, error, data: products} = useQuery(['products'], async () => {
-    console.log('fetChing....')
-    return fetch(`data/products.json`)
+  const {isLoading, error, data: products} = useQuery(['products', checked], async () => {
+    console.log('fetChing....', checked)
+    return fetch(`data/${checked ? 'sale_' : ''}products.json`)
         .then((res) => res.json())
+  }, {
+    staleTime:1000 * 60 * 5,
   })
   const handleChange = () => setChecked((prev) => !prev);
 
